7a77e29c86d5f83bc858c35e440b14e8be44f85a
Dunedain18 <Matobre@gmail.com>
2023-10-25 21:17:37 -0300

    d

--- chicasApp/src/main/java/com/mati/chicas/controladores/ImagenControlador.java
+++ chicasApp/src/main/java/com/mati/chicas/controladores/ImagenControlador.java
@@ -6,7 +6,9 @@
 package com.mati.chicas.controladores;
 
 
+import com.mati.chicas.entidades.Origen;
 import com.mati.chicas.entidades.Usuario;
+import com.mati.chicas.servicios.OrigenServicio;
 import com.mati.chicas.servicios.UsuarioServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -29,11 +31,30 @@ public class ImagenControlador {
    @Autowired
    UsuarioServicio usuarioServicio;
    
+   @Autowired
+   OrigenServicio origenServicio;
+   
    @GetMapping("/perfil/{id}")
    public ResponseEntity<byte[]> imagenUsuario (@PathVariable String id){
        Usuario usuario = usuarioServicio.getOne(id);
        
       byte[] imagen = usuario.getImagen().getContenido();
+      
+       HttpHeaders headers = new HttpHeaders();
+       
+       headers.setContentType(MediaType.IMAGE_JPEG);
+       
+       
+      
+      return new ResponseEntity<>(imagen,headers,HttpStatus.OK);
+   }
+   
+   @GetMapping("/origen/{id}")
+   public ResponseEntity<byte[]> imagenOrigen (@PathVariable String id){
+       
+       Origen origen= origenServicio.getOne(id);
+       
+      byte[] imagen = origen.getLogo().getContenido();
       
        HttpHeaders headers = new HttpHeaders();
        
